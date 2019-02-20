84e6c5aab85dc234959a237e6a0b04398213d117
erAmanpal <38061692+erAmanpal@users.noreply.github.com>
2019-02-20 22:04:41 +0530

    Create 03_Comments_statement_and_variables.cpp
    Basic of c++ programing  with variables and comments

---
+++ 03_Comments_statement_and_variables.cpp
@@ -0,0 +1,17 @@
+/* This is my sample program
+dfjki */
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int var; //declaration
+	var=17;
+	float fvar=2.3;
+	
+cout<<"var";
+cout<<var;
+cout<<fvar;
+return 0;
+}
