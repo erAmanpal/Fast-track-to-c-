483b6304912b80190847b205f6f8c22cce61427f
AmAn <38061692+erAmanpal@users.noreply.github.com>
2019-05-10 16:22:47 +0530

    Add files via upload

---
+++ 57 conversion_function.cpp
@@ -0,0 +1,24 @@
+/* converting one class type to another class type using conversion function*/
+#include<iostream>
+using namespace std;
+class cm
+{
+	int v;
+	public:
+		cm(){		}
+		cm(int n){v=n;}
+	void show(){cout<<"Equvalent cm: "<<v;	}		
+};
+
+class m
+{
+	float v;
+	public:
+		void in(){ cout<<"input value in meter : "; cin>>v;		}
+		operator cm()		{			return cm(v*100);		}
+};
+int main()
+{
+	m mo; mo.in();			cm co; 	co=mo;		co.show();
+	return 0;
+}
